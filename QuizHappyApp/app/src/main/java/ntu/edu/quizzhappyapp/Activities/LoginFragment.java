d3197c33ad2758b6e23b0437537fcc75965bb872
FOG <dn135897@gmail.com>
2024-05-20 17:08:47 +0700

    Update chức năng 'showPassword'

--- QuizHappyApp/app/src/main/java/ntu/edu/quizzhappyapp/Activities/LoginFragment.java
+++ QuizHappyApp/app/src/main/java/ntu/edu/quizzhappyapp/Activities/LoginFragment.java
@@ -7,12 +7,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.InputType;
 import android.text.TextUtils;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import ntu.edu.quizzhappyapp.Helper.QuizDBHelper;
@@ -20,19 +23,23 @@ import ntu.edu.quizzhappyapp.R;
 
 public class LoginFragment extends Fragment {
 
-    Button btn_signup;
-    Button btn_login;
+    Button btn_signup,btn_login;
+    ImageButton btn_showPwd;
     EditText username, password;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        //FindViewByID
         btn_signup = view.findViewById(R.id.btn_signup);
         btn_login = view.findViewById(R.id.btn_login_submit);
         username = view.findViewById(R.id.edit_username);
         password = view.findViewById(R.id.edt_pwd);
+        btn_showPwd = view.findViewById(R.id.showPwd);
 
+        //Xử lý sự kiện
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +52,13 @@ public class LoginFragment extends Fragment {
             }
         });
 
+        login();
+        showPassword();
+
+        return view;
+    }
+
+    public void login() {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,6 +80,36 @@ public class LoginFragment extends Fragment {
                 }
             }
         });
-        return view;
+    }
+
+    public void showPassword(){
+        btn_showPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lưu trữ các thuộc tính hiện tại của EditText
+                int inputType = password.getInputType();
+                int textSize = (int) password.getTextSize();
+                int textColor = password.getCurrentTextColor();
+                // Lưu trữ vị trí con trỏ
+                int cursorPosition = password.getSelectionStart();
+
+                // Thay đổi kiểu hiển thị của EditText
+                if ((inputType & InputType.TYPE_TEXT_VARIATION_PASSWORD) == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+                    // Nếu đang ở chế độ mật khẩu, chuyển sang chế độ text thường
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                    btn_showPwd.setImageResource(R.drawable.eye_closed);
+                } else {
+                    // Nếu đang ở chế độ text thường, chuyển sang chế độ mật khẩu
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    btn_showPwd.setImageResource(R.drawable.eye_open);
+                }
+
+                // Thiết lập lại các thuộc tính sau khi thay đổi
+                password.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+                password.setTextColor(textColor);
+                //Thiêt lập lại vị trí con trỏ
+                password.setSelection(cursorPosition);
+            }
+        });
     }
 }
--- QuizHappyApp/app/src/main/java/ntu/edu/quizzhappyapp/Activities/SignUpFragment.java
+++ QuizHappyApp/app/src/main/java/ntu/edu/quizzhappyapp/Activities/SignUpFragment.java
@@ -2,12 +2,16 @@ package ntu.edu.quizzhappyapp.Activities;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.text.InputType;
 import android.text.TextUtils;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import ntu.edu.quizzhappyapp.Helper.QuizDBHelper;
 import ntu.edu.quizzhappyapp.R;
@@ -15,6 +19,7 @@ import ntu.edu.quizzhappyapp.R;
 public class SignUpFragment extends Fragment {
 
     EditText username, password,repassword, email;
+    ImageButton btn_showPwd;
     Button signup,btn_login;
 
     @Override
@@ -23,13 +28,16 @@ public class SignUpFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
 
+        //FindViewByID
         btn_login = view.findViewById(R.id.btn_login);
         signup = view.findViewById(R.id.btn_signup_submit);
         username = view.findViewById(R.id.edt_username);
         email = view.findViewById(R.id.edt_email);
         password = view.findViewById(R.id.edt_pwd);
         repassword = view.findViewById(R.id.edt_pwd_confirm);
+        btn_showPwd = view.findViewById(R.id.showPwd);
 
+        //Xử lý sự kiện
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +49,13 @@ public class SignUpFragment extends Fragment {
             }
         });
 
+        signUp();
+        showPassword();
+
+        return view;
+    }
+
+    public void signUp(){
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,8 +92,50 @@ public class SignUpFragment extends Fragment {
                 }
             }
         });
+    }
+    public void showPassword(){
+        btn_showPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lưu trữ các thuộc tính hiện tại của EditTextPassword
+                int inputType = password.getInputType();
+                int textSize = (int) password.getTextSize();
+                int textColor = password.getCurrentTextColor();
+                // Lưu trữ vị trí con trỏ
+                int cursorPosition = password.getSelectionStart();
 
+                // Lưu trữ các thuộc tính hiện tại của EditTextRePassword
+                int inputTypeRePwd = repassword.getInputType();
+                int textSizePwd = (int) repassword.getTextSize();
+                int textColorPwd = repassword.getCurrentTextColor();
+                // Lưu trữ vị trí con trỏ
+                int cursorPositionPwd = repassword.getSelectionStart();
 
-        return view;
+                // Thay đổi kiểu hiển thị của EditText
+                if ((inputType & InputType.TYPE_TEXT_VARIATION_PASSWORD) == InputType.TYPE_TEXT_VARIATION_PASSWORD || (inputTypeRePwd & InputType.TYPE_TEXT_VARIATION_PASSWORD) == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+                    // Nếu đang ở chế độ mật khẩu, chuyển sang chế độ text thường
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                    repassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                    btn_showPwd.setImageResource(R.drawable.eye_closed);
+                } else {
+                    // Nếu đang ở chế độ text thường, chuyển sang chế độ mật khẩu
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    repassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    btn_showPwd.setImageResource(R.drawable.eye_open);
+                }
+
+                // Thiết lập lại các thuộc tính sau khi thay đổi
+                password.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+                password.setTextColor(textColor);
+                //Thiêt lập lại vị trí con trỏ
+                password.setSelection(cursorPosition);
+
+                // Thiết lập lại các thuộc tính sau khi thay đổi
+                repassword.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizePwd);
+                repassword.setTextColor(textColorPwd);
+                //Thiêt lập lại vị trí con trỏ
+                repassword.setSelection(cursorPositionPwd);
+            }
+        });
     }
 }
